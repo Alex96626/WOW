7b6cf52b656cd3df3ff0b013cbe470c478cb4f06
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-02-28 18:34:31 +0300

    правки

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-   const inputTelephoneList = document.querySelectorAll('input[type="tel"]')
+// document.addEventListener("DOMContentLoaded", () => {
+//    const inputTelephoneList = document.querySelectorAll('input[type="tel"]')
 
-   inputTelephoneList.forEach((e) => {
-        console.log(e)
-        e.maxLength = 16
-        // добавялем + в начало номера
-        e.addEventListener('focus', () => {
-            if(e.value.length  === 0){
-                e.value+='+'
-            } 
-        })
+//    inputTelephoneList.forEach((e) => {
+//         console.log(e)
+//         e.maxLength = 18
+//         e.value = '+7 (___) ___ __ __'
+//         // добавялем + в начало номера
+//         // e.addEventListener('focus', () => {
+//         //     if(e.value.length  === 0){
+//         //         e.value+='+'
+//         //     } 
+//         // })
+//         inputValueList = e.value.split('').filter((e)=> {return e !== ' '})
+//         console.log(inputValueList)
+//         e.addEventListener('input', (elem) => {
+//             console.log(e.target)   
+//         })
 
-        e.addEventListener('input', () => {
-            switch(e.value.length) {
-                case 2: 
-                    e.value +='(';
-                    break;
-                case 6:
-                    e.value +=')';
-                    break;
-                case 10:
-                    e.value +='_';
-                    break;
-                case 13:
-                    e.value +='_';
-                    break;
-            }
-        })
-    })
-});
+//         // e.addEventListener('input', () => {
+//         //     switch(e.value.length) {
+//         //         case 2: 
+//         //             e.value +='(';
+//         //             break;
+//         //         case 6:
+//         //             e.value +=')';
+//         //             break;
+//         //         case 10:
+//         //             e.value +='_';
+//         //             break;
+//         //         case 13:
+//         //             e.value +='_';
+//         //             break;
+//         //     }
+//         // })
+//     })
+// });
